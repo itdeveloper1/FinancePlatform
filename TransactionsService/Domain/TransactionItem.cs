bb42a51604ab3f37827dc571635bf5928b1a8a2a
itdeveloper1 <it_systems@live.com>
2026-02-14 17:29:46 +0200

    Update TransactionItem.cs

--- TransactionsService/Domain/TransactionItem.cs
+++ TransactionsService/Domain/TransactionItem.cs
@@ -1,1 +1,14 @@
+public class TransactionItem
+{
+    public Guid Id { get; private set; }
+    public string Description { get; private set; }
+    public decimal Amount { get; private set; }
+
+    public TransactionItem(string description, decimal amount)
+    {
+        Id = Guid.NewGuid();
+        Description = description;
+        Amount = amount;
+    }
+}
 
