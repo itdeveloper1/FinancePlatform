d1ca2edd5b3dd68275b411a48f1e95561e479b0c
itdeveloper1 <it_systems@live.com>
2026-02-14 17:11:01 +0200

    Create TransactionItem.cs

---
+++ TransactionsService/Domain/TransactionItem.cs
@@ -0,0 +1,13 @@
+public class TransactionItem
+{
+    public Guid Id { get; private set; }
+    public string Description { get; private set; }
+    public decimal Amount { get; private set; }
+
+    public TransactionItem(string description, decimal amount)
+    {
+        Id = Guid.NewGuid();
+        Description = description;
+        Amount = amount;
+    }
+}
