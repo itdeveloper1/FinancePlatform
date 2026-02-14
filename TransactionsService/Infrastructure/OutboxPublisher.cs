795419c4069ee714a982161d7edbf5afcc93f592
itdeveloper1 <it_systems@live.com>
2026-02-14 17:33:18 +0200

    Create OutboxPublisher.cs

---
+++ TransactionsService/Infrastructure/OutboxPublisher.cs
@@ -0,0 +1,22 @@
+public class OutboxPublisher
+{
+    private readonly TransactionDbContext _db;
+    private readonly IEventBus _bus;
+
+    public OutboxPublisher(TransactionDbContext db, IEventBus bus)
+    {
+        _db = db;
+        _bus = bus;
+    }
+
+    public async Task PublishPendingEvents()
+    {
+        var messages = _db.OutboxMessages.Where(m => !m.Processed).ToList();
+        foreach (var msg in messages)
+        {
+            await _bus.Publish(msg.EventType, msg.Payload);
+            msg.Processed = true;
+        }
+        await _db.SaveChangesAsync();
+    }
+}
